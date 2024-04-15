41084d917b95d82327f25f3e592820e6f5432196
박준영 <labydin@naver.com>
2024-04-15 21:14:17 +0900

    구현 예시 참고하여 필요한 함수들 추가 중

--- WishList/WishList/ViewController.swift
+++ WishList/WishList/ViewController.swift
@@ -6,27 +6,68 @@
 //
 
 import UIKit
+import CoreData
+
+
+
+struct RemoteProduct: Decodable {
+    let id: Int
+    let title: String
+    let description: String
+    let price: Double
+    let thumbnail: URL
+}
+
+
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var productImage: UIImageView!
-    @IBOutlet weak var productTitle: UILabel!
-    @IBOutlet weak var productDescription: UILabel!
-    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
     
     @IBOutlet weak var saveWishListButton: UIButton!
     @IBOutlet weak var showAnotherButton: UIButton!
     @IBOutlet weak var showWishListButton: UIButton!
+    
+    var persistentContainer: NSPersistentContainer? {
+        (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    }
+    
+    // currentProduct가 set되면, imageView. titleLabel, descriptionLabel, priceLabel에 각각 적절한 값을 지정합니다.
+    private var currentProduct: RemoteProduct? = nil {
+        didSet {
+            guard let currentProduct = self.currentProduct else { return }
+            
+            DispatchQueue.main.async {
+                self.productImage.image = nil
+                self.titleLabel.text = currentProduct.title
+                self.descriptionLabel.text = currentProduct.description
+                self.priceLabel.text = "\(currentProduct.price)$"
+            }
+            
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: currentProduct.thumbnail), let image = UIImage(data: data) {
+                    DispatchQueue.main.async { self?.productImage.image = image }
+                }
+            }
+        }
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
+        fetchRemoteProduct()
     }
-
+    
     func setUI() {
+        titleLabel.font = .boldSystemFont(ofSize: 23)
+        
         saveWishListButton.layer.cornerRadius = 10
         saveWishListButton.backgroundColor = .green
         saveWishListButton.setTitleColor(.white, for: .normal)
@@ -42,18 +83,61 @@ class ViewController: UIViewController {
     
     
     @IBAction func saveWishListButtonTapped(_ sender: UIButton) {
-        
+        saveWishProduct()
     }
     
     
     @IBAction func showAnotherButtonTapped(_ sender: UIButton) {
-        
+        fetchRemoteProduct()
     }
+    
     
     @IBAction func showWishListButtonTapped(_ sender: UIButton) {
-        
-        
+        if let wishlistVC = self.storyboard?.instantiateViewController(withIdentifier: "wishlistVC") as? WishListViewController {
+            self.present(wishlistVC, animated: true, completion: nil)
+        }
     }
     
+    
+    private func saveWishProduct() {
+        guard let context = self.persistentContainer?.viewContext else { return }
+        
+        guard let currentProduct = self.currentProduct else { return }
+        
+        let wishProduct = Product(context: context)
+        
+        wishProduct.id = Int64(currentProduct.id)
+        wishProduct.title = currentProduct.title
+        wishProduct.price = currentProduct.price
+        
+        try? context.save()
+    }
+    
+    
+    private func fetchRemoteProduct() {
+            // 1 ~ 100 사이의 랜덤한 Int 숫자를 가져옵니다.
+            let productID = Int.random(in: 1 ... 100)
+            
+            // URLSession을 통해 RemoteProduct를 가져옵니다.
+            if let url = URL(string: "https://dummyjson.com/products/\(productID)") {
+                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let data = data {
+                        do {
+                            // product를 디코드하여, currentProduct 변수에 담습니다.
+                            let product = try JSONDecoder().decode(RemoteProduct.self, from: data)
+                            self.currentProduct = product
+                        } catch {
+                            print("Decode Error: \(error)")
+                        }
+                    }
+                }
+                
+                // 네트워크 요청 시작
+                task.resume()
+            }
+        }
+
 }
 
--- WishList/WishList/WishListViewController.swift
+++ WishList/WishList/WishListViewController.swift
@@ -6,27 +6,64 @@
 //
 
 import UIKit
+import CoreData
+
+
+
+struct Product {
+    let id: Int
+    let title: String
+    let price: Double
+}
+
+
 
 class WishListViewController: UITableViewController {
 
-    
     @IBOutlet weak var tableview: UITableView!
+    
+    var persistentContainer: NSPersistentContainer? {
+            (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+        }
+    private var productList: [Product] = []
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        self.tableview.dataSource = self
+        setProductList()
     }
     
     
+    private func setProductList() {
+            guard let context = self.persistentContainer?.viewContext else { return }
+        
+            let request = Product.fetchRequest()
+        
+            if let productList = try? context.fetch(request) {
+                self.productList = productList
+            }
+        }
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        productList.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WishListTableViewCell", for: indexPath)
+            
+            let product = self.productList[indexPath.row]
+            let id = product.id
+            let title = product.title
+            let price = product.price
+            
+            cell.textLabel?.text = "[\(id)] \(title) - \(price)$"
+            
+            return cell
     }
 
     
