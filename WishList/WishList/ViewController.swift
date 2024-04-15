d0663f04b59aa7fb233f7685a7a44932c4f9111d
박준영 <labydin@naver.com>
2024-04-15 18:09:39 +0900

    create project

---
+++ WishList/WishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishList
+//
+//  Created by 박준영 on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
