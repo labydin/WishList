edc5c472a139dee8dc2151f4b3ce894bc15532a8
박준영 <labydin@naver.com>
2024-04-15 18:47:54 +0900

    컴포넌트 배치, constraints 설정

---
+++ WishList/WishList/WishListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WishListTableViewCell.swift
+//  WishList
+//
+//  Created by 박준영 on 4/15/24.
+//
+
+import UIKit
+
+class WishListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WishList/WishList/WishListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WishListViewController.swift
+//  WishList
+//
+//  Created by 박준영 on 4/15/24.
+//
+
+import UIKit
+
+class WishListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+
+}
